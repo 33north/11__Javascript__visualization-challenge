9ab1d364c49bc65b2c2f5fb2ebe54e12401641fb
admin <rmbp2015@rMBP2015-admin.local>
2022-04-29 16:39:03 -0700

    20220429 - Got rid of some code
    - Got rid of two ways for the promise data.
    // Type 1
    // d3.json(filepath).then(function(data) {
    //     console.log(data);
    //     let name = data.names;
    //     for (let i = 0; i < name.length; i++) {
    //         console.log(name[i]);
    //     };
    // });
    
    // Type 3
    // const bioDataset = d3.json(filepath).then(function(data) {console.log(data)});
    // const bioDataset = d3.json(filepath).then(function(data) {
    //     return data;
    // });
    // const moreData = bioDataset.then((data) => {
    //     // console.log(data);
    //     // let name = data.names;
    //     // for (let i = 0; i < names.length; i++) {
    //     //     console.log(names[i]);
    //     // };
    //     console.log(Promise.all(data.names));
    // });
    
    // function printstuff(data) {
    //     console.log(typeof(data));
    //     console.log(data);
    // }
    
    // printstuff(bioDataset)

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -5,53 +5,17 @@ filepath = "../data/samples.json";
 const bioData = d3.json(filepath);
 console.log("Data Promise: ", bioData);
 
-// Type 1
-// d3.json(filepath).then(function(data) {
-//     console.log(data);
-//     let name = data.names;
-//     for (let i = 0; i < name.length; i++) {
-//         console.log(name[i]);
-//     };
-// });
-
-// Type 2
-// bioData.then(function(data) {
-//     console.log(data);
-//     let name = data.names;
-//     let metadata = data.metadata;
-//     for (let i = 0; i < name.length; i++) {
-//         console.log(name[i]);
-//     };
-//     for (let i = 0; i < metadata.length; i++) {
-//         console.log(metadata[i])
-//     };
-// });
-
-// bioData.then((data) => {
-//     console.log(typeof(data));
-//     console.log(data);
-// });
-
-// Type 3
-// const bioDataset = d3.json(filepath).then(function(data) {console.log(data)});
-// const bioDataset = d3.json(filepath).then(function(data) {
-//     return data;
-// });
-// const moreData = bioDataset.then((data) => {
-//     // console.log(data);
-//     // let name = data.names;
-//     // for (let i = 0; i < names.length; i++) {
-//     //     console.log(names[i]);
-//     // };
-//     console.log(Promise.all(data.names));
-// });
-
-// function printstuff(data) {
-//     console.log(typeof(data));
-//     console.log(data);
-// }
-
-// printstuff(bioDataset)
+bioData.then(function(data) {
+    console.log(data);
+    let name = data.names;
+    let metadata = data.metadata;
+    for (let i = 0; i < name.length; i++) {
+        console.log(name[i]);
+    };
+    for (let i = 0; i < metadata.length; i++) {
+        console.log(metadata[i])
+    };
+});
 
 // function displayData(jsonFile) {
 //     jsonElement = jsonFile.names
