89138b76695483bc45713eb4463045b24c04a8f4
admin <rmbp2015@rMBP2015-admin.local>
2022-04-30 14:04:57 -0700

    20220430 - Worked on getting the app.js file
    - Worked on code for the dropdown menu and sample data values.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -39,7 +39,8 @@ bioData.then(function(data) {
 //     // Plotly.newPlot("bar", data, layout);
 // };
 
-// function getSampleData() {
+
+// function getSampleData(data) {
 //     let dropdownMenu = d3.select("#selDataset");
 //     let dataset = dropdownMenu.property("value");
 //     let data = [];
@@ -49,3 +50,42 @@ bioData.then(function(data) {
 //     }
 // }
 
+// Initializes the page with a default plot
+function init() {
+    data = [{
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 4, 8, 16] }];
+  
+    Plotly.newPlot("plot", data);
+  }
+  
+  // Call updatePlotly() when a change takes place to the DOM
+  d3.selectAll("#selDataset").on("change", updatePlotly);
+  
+  // This function is called when a dropdown menu item is selected
+  function updatePlotly() {
+    // Use D3 to select the dropdown menu
+    var dropdownMenu = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a variable
+    var dataset = dropdownMenu.property("value");
+  
+    // Initialize x and y arrays
+    var x = [];
+    var y = [];
+  
+    if (dataset === 'dataset1') {
+      x = [1, 2, 3, 4, 5];
+      y = [1, 2, 4, 8, 16];
+    }
+  
+    else if (dataset === 'dataset2') {
+      x = [10, 20, 30, 40, 50];
+      y = [1, 10, 100, 1000, 10000];
+    }
+  
+    // Note the extra brackets around 'x' and 'y'
+    Plotly.restyle("plot", "x", [x]);
+    Plotly.restyle("plot", "y", [y]);
+  }
+  
+  init();
